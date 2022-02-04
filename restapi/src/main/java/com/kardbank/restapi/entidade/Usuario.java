0b7e7273e94e56f954bc7bff198240e89e3168be
RafaelMorais <rafaelmorais.design@gmail.com>
2022-02-04 14:40:57 -0300

    Criando backend Java

---
+++ restapi/src/main/java/com/kardbank/restapi/entidade/Usuario.java
@@ -0,0 +1,54 @@
+package com.kardbank.restapi.entidade;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String cpf;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String telefone;
+    @Column(nullable = false)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
